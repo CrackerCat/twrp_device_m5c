17459eb81a60d075ad33c1f8cacac4d443f1c8ca
XRedCubeX <57260282+XRedCubeX@users.noreply.github.com>
2020-04-06 18:08:56 +0200

    Uploading Device Tree

---
+++ omni_m5c.mk
@@ -0,0 +1,19 @@
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, build/target/product/embedded.mk)
+
+# Inherit Telephony packages
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit language packages
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit 64bit support
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := m5c
+PRODUCT_NAME := omni_m5c
+PRODUCT_BRAND := meizu
+PRODUCT_MODEL := Meizu M5c
+PRODUCT_MANUFACTURER := meizu
+PRODUCT_RELEASE_NAME := Meizu M5c
